85fd3110e75172b0a4880aa1c4fbb30ba38538f2
theghost1980 <saturnob612@gmail.com>
2024-07-06 21:46:42 -0400

    first commit: webpack,ts,react,svg boilerplate

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './app.css';
+import HTML5SVG from './assets/images/svg/html5.svg';
+
+export const App = () => {
+  return (
+    <div>
+        <h1>//TODO app Monsterrat</h1>
+        <h2>Hi there in Lora typography</h2>
+        <p>I am such a nice text content using Hind Madurai!!!</p>
+        <div className='logo-svg'>
+        <HTML5SVG />
+        </div>
+    </div>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.[hash].js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+        type: "asset",
+        test: /\.(png|jpg|gif)$/i,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      //TODO below add favicon
+      //   favicon: "./src/assets/svg/saturno.svg",
+    }),
+  ],
+};

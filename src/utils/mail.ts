8753645c880320e07ec53b8637fe61825e200640
theghost1980 <saturnob612@gmail.com>
2024-08-06 08:39:20 -0400

    fixed, cleanup logs, MVP 1.0 ready

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,7 +1,6 @@
 export const checkMail = () => {
   if (window && window.localStorage) {
     const actual = localStorage.getItem("m_2024_API");
-    console.log({ actual });
     if (!actual || actual === "null") {
       localStorage.setItem("m_2024_API", JSON.stringify({ c_2024: 1 }));
     } else if (actual) {
